403838a32477293a82477bce4de8512be5f1b2a5
Joshua Galit <joshuaimalay@gmail.com>
2020-12-28 21:47:00 +0800

    Setup vue template with tailwindcss already installed

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    props: {},
+    components: {},
+    data () {
+      return {}
+    }
+  }
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms')
+  ]
+}
