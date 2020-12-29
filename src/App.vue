ae40c46cfa2b395029d6608f4e7379fb2429f548
Joshua Galit <joshuaimalay@gmail.com>
2020-12-30 06:30:10 +0800

    Setup the layout of discord using flex tailwind utility

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
-  <div id="app">
-    
+  <!-- main flex -->
+  <div id="app" class="flex flex-col min-h-screen">
+    <!-- first col -->
+    <div class="bg-blue-300 flex">
+      <div class="bg-red-300 w-56 flex-none">Tailwind CSS</div>
+      <div>Rest of top bar</div>
+    </div>
+    <!-- second col -->
+    <div class="bg-green-300 flex-1 flex">
+      <div class="bg-pink-200 w-56 flex-none">hastag sidebar</div>
+      <div class="bg-blue-100 flex-1 flex justify-between">
+        <div class="bg-green-100 flex-1">main content</div>
+        <div class="flex-none w-56 bg-yellow-100">sidebar</div>
+      </div>
+    </div>
   </div>
 </template>
 
